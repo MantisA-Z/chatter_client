f9b33834697840ebc24d8bbe56cec9b6f683c835
Mantis <160739281+MantisA-Z@users.noreply.github.com>
2025-03-11 20:45:48 +0530

    Update GroupSettings.jsx

--- src/components/GroupSettings/GroupSettings.jsx
+++ src/components/GroupSettings/GroupSettings.jsx
@@ -41,7 +41,7 @@ const GroupSettings = () => {
         setLoading(true);
         const REACT_APP_SERVER_URL = process.env.REACT_APP_SERVER_URL;
         const response = await sendReq(
-          `${REACT_APP_SERVER_URL}/signup`,
+          `${REACT_APP_SERVER_URL}/update-group`,
           {},
           JSON.stringify({ groupId })
         );
